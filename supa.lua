ddf0e785f498f970d5ebaf4ee01e6846294b2357
thesupe123 <crusherhack210buisness@gmail.com>
2026-07-01 11:20:31 +0100

    Update supa.lua

--- supa.lua
+++ supa.lua
@@ -279,7 +279,7 @@ local missilelastvelocity = Vector3.new(0,0,0)
 local localplayer = game:GetService("Players").LocalPlayer
 local target = nil
 local missile = nil
-local speed = 800
+local speed = 1500
 local VirtualInputManager = game:GetService("VirtualInputManager")
 mainheart = game:GetService("RunService").RenderStepped:Connect(function(dt)
 	if targetPlayer then
