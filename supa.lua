126f16962df23c4637bfd7168724d8a53ab31adf
thesupe123 <crusherhack210buisness@gmail.com>
2026-07-03 18:16:27 +0100

    Update supa.lua

--- supa.lua
+++ supa.lua
@@ -314,7 +314,7 @@ mainheart = game:GetService("RunService").Stepped:Connect(function(dt)
 		missilelastvelocity = missilevelocity
 
 		local direction = (calculatedtargetpos - missile.Position).Unit
-		missile.AssemblyLinearVelocity =  missile.CFrame.LookVector * speed
+		missile.AssemblyLinearVelocity =  direction * speed
 		missile.CFrame = CFrame.lookAt(missile.Position, calculatedtargetpos)
 		if (missile.Position-calculatedtargetpos).Magnitude < 20 then
 				local newdist = (missile.Position-(calculatedtargetpos+(target.Velocity*ping))).Magnitude
